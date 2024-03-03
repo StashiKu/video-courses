7cf54f666bbaca1087f3b706b69ecd1ac8519859
StashiKu <kurushinstas@gmail.com>
2024-03-04 00:08:37 +0300

    VC-29599078: implement profile page

--- src/app/features/profile/profile.module.ts
+++ src/app/features/profile/profile.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfileComponent } from 'src/app/features/profile/profile.component';
 import { RouterModule } from '@angular/router';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
 
 @NgModule({
   declarations: [ProfileComponent],
   imports: [
+    SharedModule,
     CommonModule,
     RouterModule.forChild([
       {
